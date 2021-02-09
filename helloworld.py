f24a0bf4128f7e4ff8b69bee4c9c765c8fb57bb0
hnishal <hnishal12799@gmail.com>
2021-02-09 11:44:28 +0530

    first commit

---
+++ helloworld.py
@@ -0,0 +1,13 @@
+print("hELLO, wORLD")
+if 5 > 2:
+   print("Five is greater than two!")
+x=4
+y="hey fellas"
+print(x)
+print(y)
+x = str(3)    
+y = int(3)    
+z = float(3)
+print(type(x))
+print(type(y))
+print(type(z))
